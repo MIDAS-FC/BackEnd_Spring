245234df6c92abb101796971bc9efd8a65edf49f
ehddbs4521 <ehddbs452100@naver.com>
2024-02-14 20:40:20 +0900

    first commit

---
+++ src/main/java/midas/chattly/ChattlyApplication.java
@@ -0,0 +1,13 @@
+package midas.chattly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/midas/chattly/ChattlyApplicationTests.java
@@ -0,0 +1,13 @@
+package midas.chattly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
