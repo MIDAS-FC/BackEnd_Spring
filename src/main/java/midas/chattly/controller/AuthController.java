4649631ea4881d9c59b762df10eec1e438c7abfd
ehddbs4521 <ehddbs452100@naver.com>
2024-02-25 13:43:51 +0900

    feat: Send Email(Certification Number) Controller

---
+++ src/main/java/midas/chattly/controller/AuthController.java
@@ -0,0 +1,27 @@
+package midas.chattly.controller;
+
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import midas.chattly.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/send-email")
+    public ResponseEntity<Object> sendEmail(@RequestBody HashMap<String, String> email) throws MessagingException {
+        return ResponseEntity.ok(authService.sendEmail(email.get("email")));
+    }
+
+
+}
