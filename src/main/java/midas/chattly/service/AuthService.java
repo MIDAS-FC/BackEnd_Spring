81234321aec7c946f162311aaeaa6b8fed59fae8
ehddbs4521 <ehddbs452100@naver.com>
2024-02-25 13:51:49 +0900

    feat: Verify NickName Service

--- src/main/java/midas/chattly/service/AuthService.java
+++ src/main/java/midas/chattly/service/AuthService.java
@@ -4,12 +4,15 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import midas.chattly.dto.Role;
 import midas.chattly.dto.VerifyEmailResonseDto;
+import midas.chattly.entity.User;
 import midas.chattly.repository.UserRepository;
 import midas.chattly.util.EmailUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Random;
@@ -54,6 +57,22 @@ public class AuthService {
         return verifyEmailResonseDto;
 
     }
+
+    @Transactional
+    public void verifyNickName(String nickName) {
+
+        if (userRepository.existsByNickName(nickName)) {
+            throw new RuntimeException("이미 존재하는 닉네임입니다.");
+        }
+        log.info("{}", nickName);
+        User user=User.builder()
+                .nickName(nickName)
+                .role(Role.USER.getKey())
+                .build();
+
+        userRepository.save(user);
+    }
+
 
 
 }
