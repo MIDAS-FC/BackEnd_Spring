e4851b3cbf614b997b713e3f9f63c5eb62d39a53
ehddbs4521 <ehddbs452100@naver.com>
2024-02-25 14:02:36 +0900

    feat: Check If Token Valid Or Not

--- src/main/java/midas/chattly/jwt/service/JwtService.java
+++ src/main/java/midas/chattly/jwt/service/JwtService.java
@@ -1,8 +1,7 @@
 package midas.chattly.jwt.service;
 
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
@@ -117,6 +116,22 @@ public class JwtService {
             log.error("액세스 토큰이 유효하지 않습니다.");
             return Optional.empty();
         }
+    }
+
+    public boolean isTokenValid(String token) { //토큰 검증
+        try {
+            Jwts.parser().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+        }
+        return false;
     }
 
 
