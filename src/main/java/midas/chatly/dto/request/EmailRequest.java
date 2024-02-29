a0703a784076e17f38bc4ef01935ca293084fbd9
ehddbs4521 <ehddbs452100@naver.com>
2024-02-29 23:48:50 +0900

    fix: Change Annotations

--- src/main/java/midas/chatly/dto/request/EmailRequest.java
+++ src/main/java/midas/chatly/dto/request/EmailRequest.java
@@ -1,14 +1,17 @@
 package midas.chatly.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmailRequest {
 
-    private final String email;
-    private final String emailType;
-    private final String socialType;
+    private String email;
+    private String emailType;
+    private String socialType;
 
 }
--- src/main/java/midas/chatly/dto/request/ResetPasswordRequest.java
+++ src/main/java/midas/chatly/dto/request/ResetPasswordRequest.java
@@ -3,25 +3,28 @@ package midas.chatly.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResetPasswordRequest {
 
     @Email
     @NotNull
-    private final String email;
+    private String email;
 
     @NotNull
-    private final String socialType;
+    private String socialType;
 
     @NotNull
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&]).{10,15}$",
             message = "비밀번호는 문자, 숫자, 특수문자를 포함하여 10자리 이상 15자리 이하이어야 합니다.")
-    private final String password;
+    private String password;
 
     @NotNull
-    private final String rePassword;
+    private String rePassword;
 }
