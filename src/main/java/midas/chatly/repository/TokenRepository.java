84235acf824fba6e67f7afae1c8723e5aa8d9abd
ehddbs4521 <ehddbs452100@naver.com>
2024-03-11 19:26:18 +0900

    feat: Token Repository

---
+++ src/main/java/midas/chatly/repository/TokenRepository.java
@@ -0,0 +1,15 @@
+package midas.chatly.repository;
+
+
+import midas.chatly.entity.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Long> {
+
+    Optional<Token> findByAccessToken(String accessToken);
+
+    Optional<Token> findByRefreshToken(String refreshToken);
+
+}
