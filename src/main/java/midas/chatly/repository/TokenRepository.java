a33892ec8637d9681222d4ba4c37f7aa91522a90
ehddbs4521 <ehddbs452100@naver.com>
2024-03-14 12:08:07 +0900

    fix: Refactor Repository Methods

--- src/main/java/midas/chatly/repository/TokenRepository.java
+++ src/main/java/midas/chatly/repository/TokenRepository.java
@@ -4,12 +4,10 @@ package midas.chatly.repository;
 import midas.chatly.entity.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
 
-    Optional<Token> findByAccessToken(String accessToken);
+    boolean existsByRefreshToken(String refreshToken);
 
-    Optional<Token> findByRefreshToken(String refreshToken);
 
 }
