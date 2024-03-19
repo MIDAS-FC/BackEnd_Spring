0511649578b7419da66ba819e590e6577138af17
dongyun <ehddbs452100@naver.com>
2024-03-19 16:43:34 +0900

    feat: Email Authentication Entity

---
+++ src/main/java/midas/chatly/redis/entity/EmailAuthentication.java
@@ -0,0 +1,24 @@
+package midas.chatly.redis.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+@Getter
+@AllArgsConstructor
+@RedisHash(timeToLive = 600)
+public class EmailAuthentication {
+
+    @Id
+    @Indexed
+    private String id;
+
+    private String randomNum;
+
+    @TimeToLive
+    private Long exp;
+
+}
