5428739be83295da0203996dcd2591aca6b6382c
dongyun <ehddbs452100@naver.com>
2024-03-21 19:44:46 +0900

    feat: Find By SocialId

--- src/main/java/midas/chatly/redis/repository/BlackListRepository.java
+++ src/main/java/midas/chatly/redis/repository/BlackListRepository.java
@@ -4,8 +4,12 @@ import midas.chatly.redis.entity.BlackList;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BlackListRepository extends CrudRepository<BlackList, String> {
 
     boolean existsByAccessToken(String accessToken);
+    Optional<BlackList> findBySocialId(String socialId);
+
 }
