51f8be7dc4ec9646ecd9d3384dd775a5ada69ce6
dongyun <ehddbs452100@naver.com>
2024-03-26 08:48:04 +0900

    fix: Delete UnUsed Fields;

--- src/main/java/midas/chatly/dto/request/VerifyEmailRequest.java
+++ src/main/java/midas/chatly/dto/request/VerifyEmailRequest.java
@@ -1,8 +1,9 @@
 package midas.chatly.dto.request;
 
-import lombok.*;
-
-import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
@@ -12,9 +13,6 @@ public class VerifyEmailRequest {
 
     private String email;
     private String emailType;
-    private String socialType;
-    private String randomNum;
     private String inputNum;
-    private LocalDateTime sendTime;
-    private LocalDateTime expireTime;
+    private String socialType;
 }
